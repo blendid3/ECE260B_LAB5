a5654cf9d9759121c2731cbdb0d09d5cc30426bd
Hangquan Zhao <haz023@ieng6-ece-09.ucsd.edu>
2020-02-23 18:30:23 -0800

    do

--- runext/star_wst.cmd
+++ runext/star_wst.cmd
@@ -1,14 +1,14 @@
 * StarRCXT command file for 65nm worst corner
 
-LEF_FILE: %%%%PATH_AND_FILE_NAME%%%%
+LEF_FILE: /home/linux/ieng6/ee260bwi20/public/data/libraries/lef/tcbn65gplus_8lmT2.lef
 
 LEF_USE_OBS: NO
 
-TOP_DEF_FILE: %%%%PATH_AND_FILE_NAME%%%%
+TOP_DEF_FILE: ../aes_cipher_top/aes_cipher_top.def
 
-TCAD_GRD_FILE: %%%%PATH_AND_FILE_NAME_FOR_CAP_WORST%%%%
+TCAD_GRD_FILE:  ../nxtgrd/cln65g+_1p08m+alrdl_cworst.nxtgrd
 
-MAPPING_FILE: %%%%PATH_AND_FILE_NAME%%%%
+MAPPING_FILE: ../layermap/tsmc_8lm.map
 
 EXTRACTION: RC
 
